1acf4333be77435e9f2c19317a60dd115c521778
rain <gxl405@case.edu>
2023-04-30 14:07:52 -0400

    完成所有结果数据的生成，剩下就是修改注释了

--- embMLP.py
+++ embMLP.py
@@ -1,60 +1,69 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
-
-# 读取评分数据
-ratings = pd.read_csv('./dataset/u.data', sep='\t', header=None,
-                         names=['user_id', 'movie_id', 'rating', 'timestamp'])
-
-# 编码电影和用户ID
-movie_encoder = LabelEncoder()
-user_encoder = LabelEncoder()
-ratings['movie_id'] = movie_encoder.fit_transform(ratings['movie_id'])
-ratings['user_id'] = user_encoder.fit_transform(ratings['user_id'])
-
 from keras.layers import Input, Embedding, Flatten, Dense
 from keras.models import Model
 import keras
-
-# 创建嵌入层
-num_users = ratings['user_id'].nunique()
-num_movies = ratings['movie_id'].nunique()
-embedding_size = 32
-
-user_input = Input(shape=(1,))
-user_embedding = Embedding(num_users, embedding_size)(user_input)
-user_embedding = Flatten()(user_embedding)
-
-movie_input = Input(shape=(1,))
-movie_embedding = Embedding(num_movies, embedding_size)(movie_input)
-movie_embedding = Flatten()(movie_embedding)
-
-# 将用户和电影嵌入向量进行拼接
-merged = keras.layers.Concatenate()([user_embedding, movie_embedding])
-
-# 定义MLP
-dense1 = Dense(64, activation='relu')(merged)
-dense2 = Dense(32, activation='relu')(dense1)
-output = Dense(1)(dense2)
-
 from keras.metrics import RootMeanSquaredError,MeanAbsoluteError
-# 构建模型
-model = Model(inputs=[user_input, movie_input], outputs=output)
-model.compile(loss='mse', optimizer='adam',metrics=[RootMeanSquaredError(),MeanAbsoluteError()])
 
-# 分割训练和测试集
-train_size = int(len(ratings) * 0.8)
-train_ratings = ratings[:train_size]
-test_ratings = ratings[train_size:]
-
-# 训练模型
-history = model.fit([train_ratings['user_id'], train_ratings['movie_id']], train_ratings['rating'],
-                    batch_size=64, epochs=10, verbose=1, validation_split=0.1)
-
-# 评估模型
-mse = model.evaluate([test_ratings['user_id'], test_ratings['movie_id']], test_ratings['rating'])
-
-print('Mean Squared Error: ', mse)
+# # 读取评分数据
+# ratings = pd.read_csv('./dataset/u.data', sep='\t', header=None,
+#                          names=['user_id', 'movie_id', 'rating', 'timestamp'])
+#
+# # 编码电影和用户ID
+# movie_encoder = LabelEncoder()
+# user_encoder = LabelEncoder()
+# ratings['movie_id'] = movie_encoder.fit_transform(ratings['movie_id'])
+# ratings['user_id'] = user_encoder.fit_transform(ratings['user_id'])
+#
+#
+# # 创建嵌入层
+# num_users = ratings['user_id'].nunique()
+# num_movies = ratings['movie_id'].nunique()
+# embedding_size = 32
+#
+# user_input = Input(shape=(1,))
+# user_embedding = Embedding(num_users, embedding_size)(user_input)
+# user_embedding = Flatten()(user_embedding)
+#
+# movie_input = Input(shape=(1,))
+# movie_embedding = Embedding(num_movies, embedding_size)(movie_input)
+# movie_embedding = Flatten()(movie_embedding)
+#
+# # 将用户和电影嵌入向量进行拼接
+# merged = keras.layers.Concatenate()([user_embedding, movie_embedding])
+#
+# # 定义MLP
+# dense1 = Dense(64, activation='relu')(merged)
+# dense2 = Dense(32, activation='relu')(dense1)
+# output = Dense(1)(dense2)
+#
+# # 构建模型
+# model = Model(inputs=[user_input, movie_input], outputs=output)
+# model.compile(loss='mse', optimizer='adam',metrics=[RootMeanSquaredError(),MeanAbsoluteError()])
+#
+# # 分割训练和测试集
+# train_size = int(len(ratings) * 0.8)
+# train_ratings = ratings[:train_size]
+# test_ratings = ratings[train_size:]
+#
+# result = {}
+#
+# for i in [20,40,60]:
+#     for j in [64,128,256]:
+#         # 训练模型
+#         history = model.fit([train_ratings['user_id'], train_ratings['movie_id']], train_ratings['rating'],
+#                             batch_size=j, epochs=i, verbose=1, validation_split=0.1,
+#                             callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=0)])
+#
+#         # # 评估模型
+#         # mse = model.evaluate([test_ratings['user_id'], test_ratings['movie_id']], test_ratings['rating'])
+#         result[history.history['loss'][-1]] = [i,j]
+#
+# keys = list(result.keys())
+# keys.sort()
+# min_key = keys[-1]
+# print('best para:{}'.format(result[min_key]))
 
 
 import os
@@ -74,45 +83,53 @@ mae_list = []
 import time
 startT = time.time()
 
+from keras.metrics import RootMeanSquaredError, MeanAbsoluteError
+
 for (trainset_dir, testset_dir) in folds_files:
-    trainset = pd.read_csv(trainset_dir, sep='\t', names=["UserID","MovieID","Rating","Timestamp"])
-    testset = pd.read_csv(testset_dir, sep='\t', names=["UserID","MovieID","Rating","Timestamp"])
-    train_user = trainset["UserID"].values
-    train_movie = trainset["MovieID"].values
-    cross_x_train = np.dstack((train_user, train_movie))[0]
-    cross_y_train = trainset["Rating"].values
-    x_train_input = np.transpose(cross_x_train)
+    trainset = pd.read_csv(trainset_dir, sep='\t', names=['user_id', 'movie_id', 'rating', 'timestamp'])
+    testset = pd.read_csv(testset_dir, sep='\t', names=['user_id', 'movie_id', 'rating', 'timestamp'])
 
-    test_user = testset["UserID"].values
-    test_movie = testset["MovieID"].values
-    cross_x_test = np.dstack((test_user, test_movie))[0]
-    cross_y_test = testset["Rating"].values
+    # 编码电影和用户ID
+    movie_encoder = LabelEncoder()
+    user_encoder = LabelEncoder()
+    trainset['movie_id'] = movie_encoder.fit_transform(trainset['movie_id'])
+    trainset['user_id'] = user_encoder.fit_transform(trainset['user_id'])
 
-    x_valid_input = np.transpose(cross_x_test)
+    # 创建嵌入层
+    num_users = trainset['user_id'].nunique() + testset['user_id'].nunique()
+    num_movies = trainset['movie_id'].nunique() + testset['movie_id'].nunique()
+    embedding_size = 32
 
-    num_user_train = np.max(trainset["UserID"])
-    num_movie_train = np.max(trainset["MovieID"])
+    user_input = Input(shape=(1,))
+    user_embedding = Embedding(num_users, embedding_size)(user_input)
+    user_embedding = Flatten()(user_embedding)
 
-    num_user_test = np.max(testset["UserID"])
-    num_movie_test = np.max(testset["MovieID"])
+    movie_input = Input(shape=(1,))
+    movie_embedding = Embedding(num_movies, embedding_size)(movie_input)
+    movie_embedding = Flatten()(movie_embedding)
 
-    num_user = max(num_user_train,num_user_test)
-    num_movie = max(num_movie_train,num_movie_test)
+    # 将用户和电影嵌入向量进行拼接
+    merged = keras.layers.Concatenate()([user_embedding, movie_embedding])
 
-    # train and test algorithm.
-    # 编译模型
-    best_model = Recmand_model(num_user, num_movie, 50)
+    # 定义MLP
+    dense1 = Dense(64, activation='relu')(merged)
+    dense2 = Dense(32, activation='relu')(dense1)
+    output = Dense(1)(dense2)
+
+    # 构建模型
+    model = Model(inputs=[user_input, movie_input], outputs=output)
+    model.compile(loss='mse', optimizer='adam', metrics=[RootMeanSquaredError(), MeanAbsoluteError()])
 
     # 训练模型
-    best_model.fit(cross_x_train, cross_y_train, batch_size=1000, epochs=10,
-                   callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=0)],
-                   validation_data=(cross_x_test, cross_y_test))
+    history = model.fit([trainset['user_id'], trainset['movie_id']], trainset['rating'],
+                        batch_size=64, epochs=20, verbose=1, validation_split=0.1,
+                        callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=0)])
 
     # 评估模型
-    scores = best_model.evaluate(cross_x_test, cross_y_test, verbose=0)
+    mse = model.evaluate([testset['user_id'], testset['movie_id']], testset['rating'])
 
-    rmse_list.append(scores[1])
-    mae_list.append(scores[2])
+    rmse_list.append(mse[1])
+    mae_list.append(mse[2])
 
 endT = time.time()
 import numpy as np
